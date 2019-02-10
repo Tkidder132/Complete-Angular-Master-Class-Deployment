79add9973c70670a9e292fe0c8b2c05d2eeffc8f
tkidder <no.name@google.com>
2019-02-10 03:16:22 -0600

    TS linted files

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { environment } from '../../environments/environment';
 })
 export class NavbarComponent implements OnInit {
   backgroundColor = environment.navBarBackgroundColor;
-  
+
   constructor() { }
 
   ngOnInit() {
